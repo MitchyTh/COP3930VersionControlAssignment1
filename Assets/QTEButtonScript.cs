66bd5843c376fcc87f10c8edb5195051ac9cdcd3
Mitchell Thai <mitchthai.2004@gmail.com>
2026-01-19 14:15:54 -0500

    Input system implemented: Next is mapping to buttons

--- Assets/QTEButtonScript.cs
+++ Assets/QTEButtonScript.cs
@@ -1,18 +1,63 @@
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class QTEButtonScript : MonoBehaviour
 {
-    public Animator animator;
-    public bool success;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputActionReference stepAction;
+
+    public moveSystemScript moveScript;
+
+    private bool leftSide;
+    private int correctButton;
+    private int pressedButton;
+
+    public Image player1Image, Player2Image;
+
+    //Images
+    // Player 1 (left side)
+    public Sprite[] player1Keys = new Sprite[4]; // W, A, S, D
+
+    // Player 2 (right side)
+    public Sprite[] player2Keys = new Sprite[4]; // Up, Down, Left, Right
+
+    void Awake()
     {
-        
+        leftSide = Random.Range (0, 2) == 0;
+        if (stepAction == null)
+        {
+            Debug.LogError("StepAction not assigned in Inspector", this);
+            return;
+        }
+
+        stepAction.action.Enable();
     }
 
-    // Update is called once per frame
+    public void chooseButton()
+    {
+        if (leftSide)
+        {
+            correctButton = Random.Range(0, 4);
+            player1Image.sprite = player1Keys[correctButton];
+        }
+        else
+        {
+            correctButton = Random.Range(4, 8);
+            Player2Image.sprite = player2Keys[correctButton];
+        }
+    }
     void Update()
     {
-        
+        if (stepAction != null && stepAction.action.WasPressedThisFrame())
+        {
+            moveScript.Step();
+        }
+    }
+
+    void OnDisable()
+    {
+        if (stepAction != null)
+            stepAction.action.Disable();
     }
 }
--- Assets/moveSystemScript.cs
+++ Assets/moveSystemScript.cs
@@ -5,6 +5,8 @@ public class moveSystemScript : MonoBehaviour
     public Animator animator;
     private bool walking;
     private bool success;
+    public int stepsToWin = 15;
+    private int stepsTaken = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +20,8 @@ public class moveSystemScript : MonoBehaviour
 
     public void Step()
     {
-        transform.position += new Vector3(1, 0, 0);
+        transform.position += new Vector3(0.4f, 0, 0);
+        stepsTaken++;
+        if (stepsTaken >= stepsToWin) { Debug.Log("You Win"); }
     }
 }
